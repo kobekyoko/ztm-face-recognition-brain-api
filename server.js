f0404f0e1c2439c2047197a1d743515f2ae33641
Kyoko Kobe <49667068+kobekyoko@users.noreply.github.com>
2019-05-24 14:08:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'kyoko',
+    password : '',
+    database : 'smartbrain'
+  }
+});
 
 const app = express();
 
@@ -50,29 +61,31 @@ if (req.body.email === database.users[0].email &&
 })
 
 app.post('/register', (req,res) => {
-	const { email, name, password} =req.body;
-	database.users.push({
-		id: '125',
-	 	name: name,
-	 	email: email,
-	 	entries: 0, //how many time user submitted photos
-	 	joined: new Date()
+	const { email, name, password} = req.body;
+	return db('users')
+	.returning('*')
+	    .insert({
+		email: email,
+		name: name,
+		joined: new Date()
 	})
-	res.json(database.users[database.users.length-1])
+	.then(user => {	
+	res.json(user[0]);
+	)}
+	.catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
-   	database.users.forEach(user => {
-   		if (user.id === id) {
-   			found = true;
-   			return res.json(user);
-   		} 
-   	})
-   	if (!found) {
-   		res.status(400).json('not found');
+   	db.select('*').from('users').where({id})
+     .then(user => {
+       if (user.length) {
+   	     res.json(user[0])
+   	} else {
+   		res.status(400).json('Not found')
    	}
+   	})
+     .catch(err => res.status(400).json('error getting user'))
 })
 
 app.put('/image',(req, res) => {
@@ -92,4 +105,5 @@ app.put('/image',(req, res) => {
 
 app.listen(3000, ()=> 
 	console.log('app is running on port 3000'))
+
 
